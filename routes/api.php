40998b5337c5c828266cd94fb535d4bbb3c7c896
wisnuyudiss <wisnuyudiss@gmail.com>
2022-07-05 22:04:32 +0700

    routing handler data not found

--- routes/api.php
+++ routes/api.php
@@ -25,12 +25,48 @@ Route::group(['middleware' => ['auth:api']], function () {
         return auth()->user();
     });
     //API route for MasterLokasi
-    Route::resource('lokasi', App\Http\Controllers\API\LokasiController::class);
-    Route::resource('kategori', App\Http\Controllers\API\KategoriController::class);
-    Route::resource('vendor', App\Http\Controllers\API\VendorController::class);
-    Route::resource('cost', App\Http\Controllers\API\CostController::class);
-    Route::resource('dept', App\Http\Controllers\API\DeptController::class);
-    Route::resource('condition', App\Http\Controllers\API\ConditionController::class);
+    Route::resource('lokasi', App\Http\Controllers\API\LokasiController::class)
+                    ->missing(function (Request $request) {
+                        return response()->json([
+                            'status' => '404',
+                            'message' => 'Data Not Found',
+                        ], 401); 
+                    });
+    Route::resource('kategori', App\Http\Controllers\API\KategoriController::class)
+                    ->missing(function (Request $request) {
+                        return response()->json([
+                            'status' => '404',
+                            'message' => 'Data Not Found',
+                        ], 401); 
+                    });
+    Route::resource('vendor', App\Http\Controllers\API\VendorController::class)
+                    ->missing(function (Request $request) {
+                        return response()->json([
+                            'status' => '404',
+                            'message' => 'Data Not Found',
+                        ], 401); 
+                    });
+    Route::resource('cost', App\Http\Controllers\API\CostController::class)
+                    ->missing(function (Request $request) {
+                        return response()->json([
+                            'status' => '404',
+                            'message' => 'Data Not Found',
+                        ], 401); 
+                    });
+    Route::resource('dept', App\Http\Controllers\API\DeptController::class)
+                    ->missing(function (Request $request) {
+                        return response()->json([
+                            'status' => '404',
+                            'message' => 'Data Not Found',
+                        ], 401); 
+                    });
+    Route::resource('condition', App\Http\Controllers\API\ConditionController::class)
+                    ->missing(function (Request $request) {
+                        return response()->json([
+                            'status' => '404',
+                            'message' => 'Data Not Found',
+                        ], 401); 
+                    });
     // API route for logout user
     Route::post('/logout', [App\Http\Controllers\API\AuthController::class, 'logout']);
 });
